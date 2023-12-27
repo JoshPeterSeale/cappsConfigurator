2d89017708e5626a726b00b11a5be3eaae870bdc
joshuaseale <josh.seale@outlook.com>
2023-12-27 14:17:38 +0000

    Basic front end with form working

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,11 +1,5 @@
 import logo from './cappslogo.jpg';
-import './App.css';
-import Form from'./Form.js';
-import { Dropdown } from './Dropdown.js'
-import { DropdownElement } from './DropdownElement.js'
-import { GetLengths, GetDiameters, GetPitch, GetNutType } from './GetValues.js'
-import { DebugDropdownElement } from'./DebugDropdownElement.js'
-
+import NonInterchangeableConfiguratorForm from './components/configurator-form/NonInterchangeableConfiguratorForm';
 
 function App() {
   return (
@@ -17,10 +11,7 @@ function App() {
         </p>
       </header>
       <div className="Configurator">
-        <DropdownElement label="Length:" url="length"/>
-        <DropdownElement label="Diameter:   " url="diameter" param="?length=55"/>
-        <DropdownElement label="Pitch:   " url="pitch" param="?diameter=10"/>
-        <DropdownElement label="Nut Type:   " url="nutType"/>
+        <NonInterchangeableConfiguratorForm />
       </div>
     </div>
   );
--- src/main/ui/src/index.js
+++ src/main/ui/src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider}  from 'react-redux';
+import { reducer as formReducer } from 'redux-form'
+import { combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
+import { ApolloClient, InMemoryCache, createHttpLink,  ApolloProvider, gql, from } from "@apollo/client";
+import {onError} from "@apollo/client/link/error";
+
+const reducers = {
+  form: formReducer
+}
+const reducer = combineReducers(reducers)
+const store = configureStore({reducer: reducer})
+
+const errorLink = onError(({graphqlErrors, networkErrors}) => {
+  if(graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  createHttpLink({
+    uri: "http://localhost:8080/graphql",
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:8080/graphql',
+    },
+    fetch,
+  })
+])
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
