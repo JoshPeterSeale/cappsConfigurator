84e97fdab3548f6706025851b7d3cad5118899d9
joshuaseale <josh.seale@outlook.com>
2023-12-28 15:48:05 +0000

    Adding files

---
+++ src/main/java/com/capps/cappsconfigurator/sgSeries/nonInterchangeable/model/BoltConfigurationOutput.java
@@ -0,0 +1,13 @@
+package com.capps.cappsconfigurator.sgSeries.nonInterchangeable.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BoltConfigurationOutput {
+    public String productCode;
+    public double price;
+}
---
+++ src/main/ui/src/components/configurator-form/NonInterchangeableConfiguratorOutput.jsx
@@ -0,0 +1,12 @@
+
+
+let GET_CODE_PRICE
+
+export default function NonInterchangeableConfiguratorForm({productCode}) {
+
+    return (
+        <div className="output">
+            <p> Your product code is {productCode}</p>
+        </div>
+    )
+}
